0ba5f1835c569e9e2844f7f3d69d2bc913767050
Michael <michael@radioequinoxe.com>
2026-08-10 11:53:15 +0200

    Commit LIM2 - 2026-08-10 11:53:15

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h62si5df90g"
+    "revision": "0.vjmsovs02k4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,2 +1,2 @@
-export const BUILD_TIME = "01/08/2026  8:47:35";
+export const BUILD_TIME = "10/08/2026 11:53:15";
 export const BUILD_HASH = "";
